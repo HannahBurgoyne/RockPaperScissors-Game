46e49878e0235e1f0325534579f3035cba231cc7
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2022-10-26 19:17:42 +1300

    Add comments to plan out each section of code

---
+++ javascript.js
@@ -0,0 +1,16 @@
+//write function getComputerChoice which randomly returns 'rock', 'paper' or 'scissors'
+//generate a random number between 1 and 3 
+//assign 1 to rock 
+//assign 2 to paper 
+//assign 3 to scissors
+
+
+
+//write function playRound which takes two parameters - playerSelection and computerSelection and returns loser/winner result
+//declare playerSelection
+//declare computerSelection and give it value of invoked getComputerChoice function 
+
+
+//scissors beats paper
+//paper beats rock
+//rock beats scissors
