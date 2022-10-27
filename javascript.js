00c2863b222d68c093676d4de3d852504782d766
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2022-10-27 18:43:17 +1300

    Add function to make computer select no. between 1-3

--- javascript.js
+++ javascript.js
@@ -8,7 +8,10 @@ let scissors = 3;
 
 //write function getComputerChoice which randomly returns 'rock', 'paper' or 'scissors'
 //generate a random number between 1 and 3 
-
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random()*(3)+1);
+    return computerChoice;
+}
 
 
 //write function playRound which takes two parameters - playerSelection and computerSelection and returns loser/winner result
