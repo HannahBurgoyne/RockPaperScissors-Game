9c3028aaec28b2746180f573b988f29b0dba299e
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2022-10-27 19:26:34 +1300

    Remove previous strategy of assigning a numerical value to each string

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-
-//put this part into local function to determine winner
-
-
-
 //write function getComputerChoice which randomly returns 'rock', 'paper' or 'scissors'
 //generate a random number between 1 and 3 
 function getComputerChoice() {
@@ -12,21 +7,13 @@ function getComputerChoice() {
 
 //prompt player to submit their choice 
 let playerSelection = prompt("Rock, paper or scissors?");
-//convert playerSelection string to variable
+//convert playerSelection string to lower case string
 playerSelection = playerSelection.toLowerCase();
-rock = "rock";
-paper = "paper";
-scissors = "scissors";
+
 //declare computerSelection and give it value of invoked getComputerChoice function 
 let computerSelection = getComputerChoice();
 //write function playRound which takes two parameters - playerSelection and computerSelection and returns loser/winner result
 function playRound(playerSelection, computerSelection) {
-    //assign 1 to rock 
-    let rock = 1;
-    //assign 2 to paper 
-    let paper = 2;
-    //assign 3 to scissors
-    let scissors = 3;
 
     //player winning conditionals
     if (playerSelection = "rock" && computerSelection = "scissors") {
@@ -50,8 +37,3 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-
-
-//3 beats 2
-//2 beats 1
-//1 beats 3
